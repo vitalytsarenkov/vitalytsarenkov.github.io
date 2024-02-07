e8062e01429023d679e182075a2b01a6a2c424d7
vitalytsarenkov <vitalytsarenkov@gmail.com>
2024-02-07 14:32:14 +0300

    Add a color mode toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,53 @@
-const html = document.querySelector("html");
-const menuToggle = document.querySelector(".menu-toggle");
-const menuCross = document.querySelector(".menu-cross");
+const body = document.querySelector("body");
+
+// Hide scrolling
+
+const menuToggle = body.querySelector(".menu-toggle");
+const menuCross = body.querySelector(".menu-cross");
 
 let menuCrossState = getComputedStyle(menuCross);
 
-menuToggle.addEventListener("click", () => {
-    html.classList.toggle("toggle-menu");
-    if (
-        menuCrossState.getPropertyValue("visibility") == "visible") {
-        html.style.setProperty("overflow", "hidden");
-    } else {
-        html.style.setProperty("overflow", "auto");
-    }
-});
-
-window.onresize = function () {
+function hideScroll() {
     if (
         menuCrossState.getPropertyValue("display") == "block" &&
         menuCrossState.getPropertyValue("visibility") == "visible") {
-        html.style.setProperty("overflow", "hidden");
+        body.style.setProperty("overflow", "hidden");
     } else {
-        html.style.setProperty("overflow", "auto");
+        body.style.setProperty("overflow", "auto");
     }
 };
+
+menuToggle.addEventListener("click", () => {
+    body.classList.toggle("toggle-menu");
+    hideScroll();
+});
+
+window.onresize = function () {
+    hideScroll();
+};
+
+// Toggle dark and light modes
+
+const modeToggle = body.querySelector(".mode-toggle");
+const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+const currentMode = localStorage.getItem("mode");
+if (currentMode == "dark") {
+    body.classList.toggle("dark-mode");
+} else if (currentMode == "light") {
+    body.classList.toggle("light-mode");
+}
+
+modeToggle.addEventListener("click", () => {
+    if (prefersDarkMode.matches) {
+        body.classList.toggle("light-mode");
+        var mode = body.classList.contains("light-mode") ?
+            "light" :
+            "dark";
+    } else {
+        body.classList.toggle("dark-mode");
+        var mode = body.classList.contains("dark-mode") ?
+            "dark" :
+            "light";
+    }
+    localStorage.setItem("mode", mode);
+});
